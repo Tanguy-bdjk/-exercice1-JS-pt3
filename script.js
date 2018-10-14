857dcf070ed410435dc3d9f2fb3e30385d8d0044
Tanguy-bdjk <tanguybondjokwe@gmail.com>
2018-10-14 14:11:19 +0200

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+// function bordure(){
+//   document.getElementById('image1').style.border='solid 3px red';
+// }http://static8.viadeo-static.com/3x-WHN2Qie3RZL-heplhttp://static8.viadeo-static.com/3x-WHN2Qie3RZL-heplZFykbLwI=/300x300/member/002gn198g54g8t8%3Fts%3D1458640849000ZFykbLwI=/300x300/member/002gn198g54g8t8%3Fts%3D1458640849000
+//
+// function bordure(){
+//   document.getElementById('image1').style.border='none';
+// }
+function bordure(){
+  document.getElementById('image1').style.border='solid 3px red';
+}
+function enlever(){
+  document.getElementById('image1').style.border='none';
+}
